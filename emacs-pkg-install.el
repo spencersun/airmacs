c464c6365f14e26af1dbd630b48996bdb60c7d63
S. Spencer Sun <spencer89123@gmail.com>
2020-05-16 18:28:23 -0700

    Changes I had to make to get this to work on my Chromebook's Linux container - workaround some TLS issue downloading GNU archive - Marmalade repo is discontinued https://www.emacswiki.org/emacs/MarmaladeRepo
    Misc:
    - switch melpa to https for secure feels
    - use --script and pass all the packages in a single invocation
      for less overhead

--- emacs-pkg-install.el
+++ emacs-pkg-install.el
@@ -7,18 +7,19 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Fix HTTP1/1.1 problems
 (setq url-http-attempt-keepalives nil)
 
+;; emacs 26: Failed to download ‘gnu’ archive.
+;; https://emacs.stackexchange.com/a/51772
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (package-initialize)
+(package-refresh-contents)
 (message "Installing packages:")
-(loop for package in pkgs-to-install
-      do
-      (progn
-        (message (format "\t%s" package)))
-        (package-install package))
+(dolist (pkg argv)
+  (if (package-installed-p pkg)
+      (message (format "\t%s is already installed" pkg))
+    (package-install (intern pkg) t)))
--- emacs-pkg-install.sh
+++ emacs-pkg-install.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-emacs --batch --eval "(package-refresh-contents)"
-for pkg in $*; do
-    emacs --batch --eval "(setq pkgs-to-install '($pkg))" -l emacs-pkg-install.el
-done;
+emacs --script emacs-pkg-install.el "$@"
